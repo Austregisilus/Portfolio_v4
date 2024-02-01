05c809ac6b1860d3066888ceae857d5c29cca866
Austregisilus <karsenio13@gmail.com>
2024-02-01 23:25:51 +0300

    correcting layout

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -5,8 +5,9 @@ import { IoAnalyticsOutline } from "react-icons/io5";
 import analytics from "../../public/google-data-analytics-certificate.png";
 import developer from "../../public/meta-front-end-developer-certificate.png";
 import { MdOutlineDesignServices, MdDeveloperMode } from "react-icons/md";
-import { Chip } from "@nextui-org/react";
+import { Chip, Divider } from "@nextui-org/react";
 import CardTilt from "./UI/CardTilt";
+import { twMerge } from "tailwind-merge";
 
 const Services = () => {
   const heading =
@@ -14,10 +15,10 @@ const Services = () => {
   const content =
     "font-Gudea my-2 text-center text-base text-white/60 md:text-lg";
   const main = "font-Gudea my-2 text-base text-white/60 md:text-lg";
-  const mainCard =
-    "font-Gudea my-4 border-b-[1px] border-neutral-900 py-4 text-base text-white/60 md:text-lg";
+  const mainCard = "font-Gudea my-1 py-1 text-base text-white/60 md:text-lg";
   const head =
     "font-Roboto my-4 text-2xl font-bold capitalize text-white md:text-3xl";
+  const chip = "font-Gudea my-2 mr-1 bg-purple-900 text-sm md:text-base";
 
   return (
     <section className="my-4">
@@ -25,7 +26,8 @@ const Services = () => {
         <div className="grid place-content-between">
           <div className="relative mt-4 flex flex-col justify-center py-10">
             <h1 className="font-Fira mb-2 w-full pb-2 text-4xl font-bold capitalize md:text-5xl lg:text-6xl lg:leading-[50px]">
-              Skills ranging from data analysis to web development.
+              Skills ranging from data <br /> analysis to web <br />{" "}
+              development.
             </h1>
             <p className={main}>
               During my studies, I developed skills in front-end development,
@@ -99,7 +101,7 @@ const Services = () => {
                     transform: "translateZ(50px)",
                   }}
                 >
-                  Frontend developer badge
+                  Front-end developer badge
                 </h6>
                 <p
                   className={content}
@@ -139,41 +141,47 @@ const Services = () => {
               Analyzes data to uncover insights and trends providing valuable
               information for decision making and business strategies. Able to
               use tools such as:
-              <br />
-              <Chip
-                className="font-Gudea my-4 mr-1 bg-purple-900 py-2 text-sm md:text-base"
-                style={{
-                  transform: "translateZ(50px)",
-                }}
-              >
-                Python
-              </Chip>
-              <Chip
-                className="font-Gudea my-4 mr-1 bg-purple-900 py-2 text-sm md:text-base"
-                style={{
-                  transform: "translateZ(50px)",
-                }}
-              >
-                Microsoft Excel
-              </Chip>
-              <Chip
-                className="font-Gudea my-4 mr-1 bg-purple-900 py-2 text-sm md:text-base"
-                style={{
-                  transform: "translateZ(50px)",
-                }}
-              >
-                R language
-              </Chip>
-              <Chip
-                className="font-Gudea my-4 mr-1 bg-purple-900 py-2 text-sm md:text-base"
-                style={{
-                  transform: "translateZ(50px)",
-                }}
-              >
-                Power BI
-              </Chip>
             </p>
+            <Chip
+              className={chip}
+              style={{
+                transform: "translateZ(50px)",
+              }}
+            >
+              Python
+            </Chip>
+            <Chip
+              className={chip}
+              style={{
+                transform: "translateZ(50px)",
+              }}
+            >
+              Microsoft Excel
+            </Chip>
+            <Chip
+              className={chip}
+              style={{
+                transform: "translateZ(50px)",
+              }}
+            >
+              R language
+            </Chip>
+            <Chip
+              className={chip}
+              style={{
+                transform: "translateZ(50px)",
+              }}
+            >
+              Power BI
+            </Chip>
           </div>
+          <Divider
+            style={{
+              transform: "translateZ(50px)",
+            }}
+            orientation="horizontal"
+            className="my-2 bg-neutral-800"
+          />
           <div
             style={{
               transform: "translateZ(50px)",
@@ -182,46 +190,52 @@ const Services = () => {
             <div className="mb-3 mt-2 flex h-20 w-20 items-center justify-center rounded-full bg-black pb-2 pt-2 ring-[1px] ring-neutral-800">
               <MdDeveloperMode size={50} className="text-6xl text-blue-800" />
             </div>
-            <h3 className={head}>Front end developer</h3>
+            <h3 className={head}>Front-end developer</h3>
             <p className={mainCard}>
               Builds the visual and interactive elements of a website or
               application using HTML, CSS and Javascript. Able to use tools such
               as:
-              <br />
-              <Chip
-                className="font-Gudea my-4 mr-1 bg-blue-900 py-2 text-sm md:text-base"
-                style={{
-                  transform: "translateZ(50px)",
-                }}
-              >
-                HTML
-              </Chip>
-              <Chip
-                className="font-Gudea my-4 mr-1 bg-blue-900 py-2 text-sm md:text-base"
-                style={{
-                  transform: "translateZ(50px)",
-                }}
-              >
-                CSS
-              </Chip>
-              <Chip
-                className="font-Gudea my-4 mr-1 bg-blue-900 py-2 text-sm md:text-base"
-                style={{
-                  transform: "translateZ(50px)",
-                }}
-              >
-                JavaScript
-              </Chip>
-              <Chip
-                className="font-Gudea my-4 mr-1 bg-blue-900 py-2 text-sm md:text-base"
-                style={{
-                  transform: "translateZ(50px)",
-                }}
-              >
-                React
-              </Chip>
             </p>
+            <Chip
+              className={twMerge(chip, "bg-blue-800")}
+              style={{
+                transform: "translateZ(50px)",
+              }}
+            >
+              HTML
+            </Chip>
+            <Chip
+              className={twMerge(chip, "bg-blue-800")}
+              style={{
+                transform: "translateZ(50px)",
+              }}
+            >
+              CSS
+            </Chip>
+            <Chip
+              className={twMerge(chip, "bg-blue-800")}
+              style={{
+                transform: "translateZ(50px)",
+              }}
+            >
+              JavaScript
+            </Chip>
+            <Chip
+              className={twMerge(chip, "bg-blue-800")}
+              style={{
+                transform: "translateZ(50px)",
+              }}
+            >
+              React
+            </Chip>
           </div>
+          <Divider
+            style={{
+              transform: "translateZ(50px)",
+            }}
+            orientation="horizontal"
+            className="my-2 bg-neutral-800"
+          />
           <div
             style={{
               transform: "translateZ(50px)",
@@ -234,28 +248,27 @@ const Services = () => {
               />
             </div>
             <h3 className={head}>UI/UX designer</h3>
-            <p className="font-Gudea my-4 py-4 text-base text-white/60 md:text-lg">
+            <p className={mainCard}>
               Focuses on creating user-friendly interfaces and enhancing user
               experiences through research, wireframing and visual design. Able
               to use tools such as:
-              <br />
-              <Chip
-                className="font-Gudea my-4 mr-1 bg-emerald-900 py-2 text-sm md:text-base"
-                style={{
-                  transform: "translateZ(50px)",
-                }}
-              >
-                Figma
-              </Chip>
-              <Chip
-                className="font-Gudea my-4 mr-1 bg-emerald-900 py-2 text-sm md:text-base"
-                style={{
-                  transform: "translateZ(50px)",
-                }}
-              >
-                WebFlow
-              </Chip>
             </p>
+            <Chip
+              className={twMerge(chip, "bg-emerald-800")}
+              style={{
+                transform: "translateZ(50px)",
+              }}
+            >
+              Figma
+            </Chip>
+            <Chip
+              className={twMerge(chip, "bg-emerald-800")}
+              style={{
+                transform: "translateZ(50px)",
+              }}
+            >
+              WebFlow
+            </Chip>
           </div>
         </CardTilt>
       </div>
